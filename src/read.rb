dab3069d2292c6dfd5d133e3ff4a972ff511dc1d
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2014-10-04 02:34:23 +0900

    learn images

--- src/read.rb
+++ src/read.rb
@@ -1,19 +1,19 @@
-require 'jubatus'
+require 'jubatus/classifier/client'
 
 NUM = 5
 class Meal
   def initialize(host='127.0.0.1', port=9199)
-    @jubatus = Jubatus::Classifier::Client::Classifier.new('127.0.0.1', 9199)
+    @jubatus = Jubatus::Classifier::Client::Classifier.new(host, port)
   end
 
   def train(label, data)
     rn = 0
     begin
-      datum = Jubatus::Common::Datum.new(data)
+      datum = Jubatus::Common::Datum.new
+      datum.add_binary(data)
       @jubatus.train(datum)
     rescue MessagePack::RPC::TimeoutError
       if rn < NUM
-        sleep
         rn += 1
         retry
       end
@@ -22,7 +22,34 @@ class Meal
 
   def read_image(path)
     label = path.split('/')[-2]
-    data = {image: File.open(path).read()}
+    data = File.open(path).read()
     [label, data]
+  end
+end
+
+m = Meal.new
+path = 'images'
+
+option = ARGV[0]
+if option == 'train'
+  Dir.entries(path).each do |dir|
+    p = [path, dir].join('/')
+    if File.directory?(p) && dir =~ /^\./
+      Dir.entries(p).each do |jpg|
+        if jpg =~ /^\./
+          label, data = m.read_image([p,jpg].join('/'))
+          m.train(label, data)
+        end
+      end
+    end
+  end
+else
+  Dir.entries(path).each do |file|
+    p = [path, file].join('/')
+    unless File.directory?(p)
+      image = m.read_image(p)[1]
+      r = m.classify(image)
+      m.result(r)
+    end
   end
 end
